a7b326be618f3d3d6dfc340945cdb25dc3498d5f
haot <149553316+haot2025@users.noreply.github.com>
2026-06-01 20:32:23 +0100

    Update _layers.py

--- models/_layers.py
+++ models/_layers.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022 The torch-harmonics Authors. All rights reserved.
-# SPDX-FileCopyrightText: Copyright (c) 2025 The GSFNO Authors. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 The GSNO Authors. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
 import torch
